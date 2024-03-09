527333d0371200586e1f38d6ea9c7b49530792be
kirill-jk <ikurilyuk4@gmail.com>
2024-03-09 13:58:30 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Кирилл Курилюк on 09.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
